8753169f32691227abf0b5bbb566a0c278ad8403
Căbău Natan <cabau.natan.dev@gmail.com>
2023-09-11 14:17:33 +0300

    StartMap

--- Assets/Mechanics/Map/GroundFollow.cs
+++ Assets/Mechanics/Map/GroundFollow.cs
@@ -1,29 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class GroundFollow : MonoBehaviour
 {
-    private Transform Player; // Reference to the player's Transform component.
+    private Transform Player;
     public float SmoothSpeed = 5.0f; // The speed at which the camera follows the player.
-
-    private Vector3 Offset; // The initial offset between the player and the camera.
-
-    void Start()
-    {   
+    private void Start()
+    {
         //Get the player from Game Manager
         Player = GameManager.Instance.MainPlayer.transform;
-        // Calculate the initial offset between the player and the camera.
-        Offset = transform.position - Player.position;
-
     }
-
     void Update()
-    {
+    { 
         FollowPlayer();
     }
     public void FollowPlayer()
     {
         // Calculate the desired camera position based on the player's Y and Z positions.
-        Vector3 targetPosition = Player.position + Offset;
+        Vector3 targetPosition = new Vector3(transform.position.x, transform.position.y, Player.position.z);
 
         // Use Mathf.SmoothDamp to smoothly interpolate between the current camera position and the target position.
         transform.position = Vector3.Lerp(transform.position, targetPosition, SmoothSpeed * Time.fixedDeltaTime);
